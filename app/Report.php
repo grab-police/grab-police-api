71f797c509f857fa7e17571549c689cbaad14293
Ed Ryan Alburo <edryanalburo@coredev.ph>
2016-01-09 17:45:39 +0800

    set created_at date attribute to time ago config

--- app/Report.php
+++ app/Report.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
@@ -9,4 +10,9 @@ class Report extends Model
     protected $fillable  = ['shortdesc','description','area'];
 
     protected $table     = 'reports';
+
+    public function getCreatedAtAttribute($date=array())
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s',$date)->diffForHumans();
+    }
 }
